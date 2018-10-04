9fef0e346148191d989143a70976344e9396a943
fedeLuque <federico.luquee@gmail.com>
2018-10-03 22:20:52 -0300

    Agregado de hojas de estilos

--- ej11.js
+++ ej11.js
@@ -5,6 +5,10 @@ const path = require('path')
 
 app.get('/', (req, res) => res.sendFile (path.join(__dirname, 'index.html')))
 
-app.get('/productos', (req, res) => res.sendFile (path.join(__dirname, 'productos.html')))
+app.get('/productos.html', (req, res) => res.sendFile (path.join(__dirname, 'productos.html')))
+
+app.get('/css/lib/bootstrap.min.css', (req, res) => res.sendFile (path.join(__dirname, 'css/lib/bootstrap.min.css')))
+
+app.get('/css/styles.css', (req, res) => res.sendFile (path.join(__dirname, 'css/styles.css')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
